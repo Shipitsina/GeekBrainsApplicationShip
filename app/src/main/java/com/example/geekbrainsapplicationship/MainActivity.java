20bed2e08ac1e722901c1a71e8e3d4d5537cb36e
Shipitsina <kybeg@inbox.ru>
2021-07-07 17:01:43 +0500

    LESSON1.1

--- app/src/main/java/com/example/geekbrainsapplicationship/MainActivity.java
+++ app/src/main/java/com/example/geekbrainsapplicationship/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.geekbrainsapplicationship;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.textView);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn.setTextSize(20);
+                tv.setText("ЖМЯК!!!");
+                tv.setTextSize((float) Math.random()*100 + 5);
+                tv.setBackgroundColor(Color.parseColor("#FF0000"));
+            }
+        });
     }
 }
