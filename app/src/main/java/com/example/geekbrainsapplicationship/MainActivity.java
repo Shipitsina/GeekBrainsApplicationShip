821508c78a891bcf8c974e495fcdda8c8a743461
Shipitsina <kybeg@inbox.ru>
2021-07-08 21:57:47 +0500

    done homework#2

--- app/src/main/java/com/example/geekbrainsapplicationship/MainActivity.java
+++ app/src/main/java/com/example/geekbrainsapplicationship/MainActivity.java
@@ -10,31 +10,12 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tv;
-    Button btn1, btn2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv = (TextView) findViewById(R.id.textView);
-        btn1 = (Button) findViewById(R.id.button);
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                btn1.setTextSize(20);
-                tv.setText("ЖМЯК!!!");
-                tv.setTextSize((float) Math.random()*100 + 5);
-                tv.setBackgroundColor(Color.parseColor("#FF0000"));
-            }
-
-        });
-
-        btn2 = (Button) findViewById(R.id.btnStartActivity2);
-        btn2.setOnClickListener(v -> {
-            Intent intent = new Intent(v.getContext(), MainActivity2.class);
-            startActivity(intent);
-        });
     }
 
 
